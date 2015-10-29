16adb2ee3d4acf87c7eec3b4478274e3f6f588cd
kk_Ataka <kk_Ataka@ring.skr.jp>
2015-10-29 13:20:52 +0900

    delete submodule

---
+++ README.md
@@ -0,0 +1,4 @@
+![](https://circleci.com/gh/gosyujin/be-side.svg?style=shield&circle-token=23509828f17c2717bb5f6884435787a50884964e)
+
+- sourceはWikiと連動している
+- masterをpushしたら [CircleCI](https://circleci.com/gh/gosyujin/be-side/tree/master) でgh-pagesへデプロイ
---
+++ script/description_parser.rb
@@ -0,0 +1,58 @@
+require 'open-uri'
+require 'nokogiri'
+
+Url = 'http://be-side.jp/podcast'
+
+File_url_prefix = 'http://project-phi.ddo.jp/ishikawa/ishikawa'
+File_url_suffix = '.mp3'
+
+def parse(url)
+  begin
+    doc = Nokogiri::HTML.parse(open(url))
+  rescue OpenURI::HTTPError
+    return
+  end
+
+  current_vol = 0
+
+  doc.xpath('//div[@class="text"]').each do |node|
+    back_number = node.at("a").attribute("href").value
+    back_number.slice!(File_url_prefix)
+    back_number.slice!(File_url_suffix)
+
+    vol = back_number.split("_")[0]
+
+    description = node.content.strip!.split("\n")
+
+    unless current_vol == vol
+      puts '第' + vol + '回'
+      puts '========'
+      puts ''
+      current_vol = vol
+    end
+
+    puts 'vol.' + back_number + ' ' + description[0][0..30] + '...'
+    puts '----------' + ('--' * description[0][0..30].length) + '---'
+    puts ''
+    puts '::'
+    puts ''
+    description.each do |d|
+       puts '   ' + d
+    end
+    puts ''
+  end
+end
+
+y = ARGV[0].to_i
+
+y.downto(y) do |year|
+  puts '======'
+  puts year.to_s + '年'
+  puts '======'
+  puts ''
+  12.downto(1) do |month|
+    parse(Url + "/" + year.to_s + "/" + ("%02d" % month).to_s + "/")
+  end
+end
+
+
